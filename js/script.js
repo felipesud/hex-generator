22b0cb48f744491eebbc25c9d8fbdab15f6e6a86
Felipe Belisário <felipesantos.dsb@hotmail.com>
2022-01-06 13:23:03 -0300

    update files

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-function myColor(){
+function myColor() {
     var color = document.getElementById('colorPanel').value;
     document.body.style.backgroundColor = color;
-    
+    document.getElementById('box').value = color;
+
 };
